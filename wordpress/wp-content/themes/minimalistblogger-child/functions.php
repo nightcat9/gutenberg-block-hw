df73c1beaf938225fe89906d3877323e5b207df5
Felicia Hoeft <fhoeft92615@gmail.com>
2023-02-22 19:05:33 -0600

    changed edit settings

--- wordpress/wp-content/themes/minimalistblogger-child/functions.php
+++ wordpress/wp-content/themes/minimalistblogger-child/functions.php
@@ -36,4 +36,10 @@ add_action('after_setup_theme', function (){
             'color' => '#008080',
         ),
     ) );
+
+    add_theme_support('editor-styles');
+    add_editor_style('css/editor.css');
+
+    add_theme_support('disable-custom-colors');
+
 }, 100);
